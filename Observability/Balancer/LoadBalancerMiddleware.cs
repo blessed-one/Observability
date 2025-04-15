ec6a13061cd72bd1a55d41658c64f1a02bd4e811
Abdulloyev Shakhzod <abdulloev-2005@mail.ru>
2025-04-15 20:04:18 +0300

    did some changes

--- Observability/Balancer/LoadBalancerMiddleware.cs
+++ Observability/Balancer/LoadBalancerMiddleware.cs
@@ -1,94 +1,34 @@
-﻿using System.Buffers;
 
 namespace Balancer
 {
-    public class LoadBalancerMiddleware(ILoadBalancer loadBalancer) : IMiddleware
+    public class LoadBalancerMiddleware : IMiddleware
     {
-        private readonly HttpClient _httpClient = new();
-        private readonly Dictionary<string, string[]> _serviceEndpoints = new()
+        private readonly ILoadBalancer _loadBalancer;
+
+        public LoadBalancerMiddleware(ILoadBalancer loadBalancer)
         {
-            {"first-service", Environment.GetEnvironmentVariable("FIRST_SERVICE_ENDPOINTS")?.Split(',') 
-                              ?? Array.Empty<string>() },
-            {"second-service", Environment.GetEnvironmentVariable("SECOND_SERVICE_ENDPOINTS")?.Split(',') 
-                               ?? Array.Empty<string>()}
-        };
+            _loadBalancer = loadBalancer;
+        }
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            try
+            var targetSever = _loadBalancer.GetNextServer();
+
+            var targetUri = new Uri(targetSever);
+            context.Request.Host = new HostString(targetUri.Host);
+            context.Request.Scheme = targetUri.Scheme;
+            context.Request.PathBase = targetUri.LocalPath;
+
+            using var httpClient = new HttpClient();
+            var responce = await httpClient.SendAsync(new HttpRequestMessage
             {
-                var targetService = GetTargetService(context.Request.Path.Value!);
-                
-                if (targetService is null)
-                    return;
-                var targetUri = loadBalancer.GetNextServiceServer(targetService);
+                Method = new HttpMethod(context.Request.Method),
+                RequestUri = new Uri(targetUri, context.Request.Path),
+                Content = new StreamContent(context.Request.Body)
+            });
 
-                var request = new HttpRequestMessage
-                {
-                    Method = new HttpMethod(context.Request.Method),
-                    RequestUri = new Uri(targetUri, context.Request.Path)
-                };
-
-                if (context.Request.Body.CanRead)
-                {
-                    var content = await context.Request.BodyReader.ReadAsync();
-                    request.Content = new ByteArrayContent(content.Buffer.ToArray());
-                }
-
-                foreach (var header in context.Request.Headers)
-                {
-                    var isContentHeader = header.Key.StartsWith("Content-", StringComparison.OrdinalIgnoreCase)
-                        || header.Key.Equals("ContentType", StringComparison.OrdinalIgnoreCase)
-                        || header.Key.Equals("Content-Length", StringComparison.OrdinalIgnoreCase);
-
-                    if (isContentHeader)
-                    {
-                        request.Content ??= new ByteArrayContent([]);
-                        request.Content.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
-                    }
-                    else
-                    {
-                        request.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
-                    }
-                }
-
-                Console.WriteLine("Sending request...");
-                var response = await _httpClient.SendAsync(request);
-
-                context.Response.StatusCode = (int)response.StatusCode;
-                
-                foreach (var header in response.Headers)
-                {
-                    if (!header.Key.Equals("Transfer-Encoding", StringComparison.OrdinalIgnoreCase))
-                        context.Response.Headers[header.Key] = header.Value.ToArray();
-                }
-
-                foreach (var header in response.Content.Headers)
-                {
-                    if (!header.Key.Equals("Transfer-Encoding", StringComparison.OrdinalIgnoreCase))
-                        context.Response.Headers[header.Key] = header.Value.ToArray();
-                }
-
-                context.Response.ContentType = response.Content.Headers.ContentType?.ToString();
-                context.Response.ContentLength = response.Content.Headers.ContentLength;
-
-                await response.Content.CopyToAsync(context.Response.Body);
-                await context.Response.Body.FlushAsync();
-            }
-            catch (Exception ex)
-            {
-                context.Response.StatusCode = 503;
-                await context.Response.WriteAsync("Service unavailable");
-                Console.WriteLine("Error processing request");
-                Console.WriteLine(ex.Message);
-            }
-        }
-        
-        private string? GetTargetService(string path)
-        {
-            return (from service in _serviceEndpoints 
-                where service.Value.Any(endpoint => endpoint.Equals(path)) 
-                select service.Key).FirstOrDefault();
+            context.Response.StatusCode = (int)responce.StatusCode;
+            await responce.Content.CopyToAsync(context.Response.Body);
         }
     }
 }
